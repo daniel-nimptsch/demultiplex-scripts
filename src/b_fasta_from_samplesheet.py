fe4a052632f21b4be1f7da25933c3709b0e4a727
Daniel Nimptsch <danielnimptsch@hotmail.de>
2024-06-14 08:59:39 +0200

    Refactored code by removing unnecessary whitespace.

--- src/b_fasta_from_samplesheet.py
+++ src/b_fasta_from_samplesheet.py
@@ -3,9 +3,9 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description="Process a samplesheet in CSV format.")
     parser.add_argument('samplesheet', type=str, help='Path to the samplesheet CSV file')
-    
+
     args = parser.parse_args()
-    
+
     samplesheet = args.samplesheet
     # Add your processing logic here
 
