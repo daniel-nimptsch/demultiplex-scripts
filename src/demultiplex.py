4ba2c2420353319b1d9961c45f9b37d97dd930c4
Daniel Nimptsch <danielnimptsch@hotmail.de>
2024-06-25 10:34:34 +0200

    Refactored cutadapt command execution to a separate function.

--- src/demultiplex.py
+++ src/demultiplex.py
@@ -3,21 +3,7 @@ import os
 import subprocess
 
 
-def run_cutadapt(forward_fasta, reverse_fasta, fq_gz_1, fq_gz_2, output_dir):
-    """
-    Execute the cutadapt command with the given parameters.
-    """
-    command = (
-        f"cutadapt -e 2 --pair-adapters --cores=0 "
-        f"-g ^file:{forward_fasta} "
-        f"-G ^file:{reverse_fasta} "
-        f"-o '{output_dir}/demux-{{name}}_R1.fastq.gz' "
-        f"-p '{output_dir}/demux-{{name}}_R2.fastq.gz' "
-        f"{fq_gz_1} "
-        f"{fq_gz_2} "
-        f"--revcomp "
-    )
-
+def run_command(command, output_dir):
     log_file = os.path.join(output_dir, "cutadapt.log")
     with open(log_file, "w") as log:
         process = subprocess.Popen(
@@ -27,6 +13,37 @@ def run_cutadapt(forward_fasta, reverse_fasta, fq_gz_1, fq_gz_2, output_dir):
             print(line.decode(), end="")
             log.write(line.decode())
         process.wait()
+
+
+def run_cutadapt(
+    forward_fasta, reverse_fasta, fq_gz_1, fq_gz_2, combinatorial, output_dir
+):
+    """
+    Execute the cutadapt command with the given parameters.
+    """
+    if combinatorial:
+        command = (
+            f"cutadapt -e 0.15 --no-indels --cores=0 "
+            f"-g ^file:{forward_fasta} "
+            f"-G ^file:{reverse_fasta} "
+            f"-o '{output_dir}/demux-{{name1}}-{{name2}}_R1.fastq.gz' "
+            f"-p '{output_dir}/demux-{{name1}}-{{name2}}_R2.fastq.gz' "
+            f"{fq_gz_1} "
+            f"{fq_gz_2} "
+            f"--revcomp "
+        )
+    else:
+        command = (
+            f"cutadapt -e 2 --pair-adapters --cores=0 "
+            f"-g ^file:{forward_fasta} "
+            f"-G ^file:{reverse_fasta} "
+            f"-o '{output_dir}/demux-{{name}}_R1.fastq.gz' "
+            f"-p '{output_dir}/demux-{{name}}_R2.fastq.gz' "
+            f"{fq_gz_1} "
+            f"{fq_gz_2} "
+        )
+
+    run_command(command)
 
 
 def main():
