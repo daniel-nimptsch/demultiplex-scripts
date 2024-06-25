f64889a0ac83751ef93a04e2c9590e9b3b778afc
Daniel Nimptsch <danielnimptsch@hotmail.de>
2024-06-25 08:52:40 +0200

    format and comments

--- src/b_fasta_from_samplesheet.py
+++ src/b_fasta_from_samplesheet.py
@@ -25,57 +25,61 @@ def main():
     parser = argparse.ArgumentParser(
         description="Samplesheet (TSV) to FASTA converter with forward and reverse barcodes for demultiplexing with cutadapt."
     )
+    parser.add_argument("fq_gz_1", type=str, help="Path to the first FASTQ file (R1)")
+    parser.add_argument("fq_gz_2", type=str, help="Path to the second FASTQ file (R2)")
     parser.add_argument(
-        'fq_gz_1', 
-        type=str, 
-        help='Path to the first FASTQ file (R1)'
+        "-o",
+        "--output",
+        type=str,
+        default=".",
+        help="Directory to save the output FASTA files (default: ./)",
     )
     parser.add_argument(
-        'fq_gz_2', 
-        type=str, 
-        help='Path to the second FASTQ file (R2)'
-    )
-    parser.add_argument(
-        '-o', '--output', 
-        type=str, 
-        default='.', 
-        help='Directory to save the output FASTA files (default: ./)'
-    )
-    parser.add_argument(
-        'samplesheet', 
-        type=str, 
-        help='Path to the samplesheet TSV containing sample names and barcodes'
+        "samplesheet",
+        type=str,
+        help="Path to the samplesheet TSV containing sample names and barcodes",
     )
 
     args = parser.parse_args()
 
     samplesheet = args.samplesheet  # Get the samplesheet file path from arguments
     output_dir = args.output  # Get the output directory from arguments
-    os.makedirs(output_dir, exist_ok=True)  # Create the output directory if it doesn't exist
+    os.makedirs(
+        output_dir, exist_ok=True
+    )  # Create the output directory if it doesn't exist
 
     fq_gz_1 = args.fq_gz_1  # Get the first FASTQ file path from arguments
     fq_gz_2 = args.fq_gz_2  # Get the second FASTQ file path from arguments
 
-    with open(samplesheet, mode='r') as file:  # Open the samplesheet file
-        csv_reader = csv.reader(file, delimiter='\t')  # Read the file as a TSV
+    with open(samplesheet, mode="r") as file:  # Open the samplesheet file
+        csv_reader = csv.reader(file, delimiter="\t")  # Read the file as a TSV
         barcodes = []  # Initialize a list to store barcodes
 
         for row in csv_reader:  # Iterate over each row in the TSV
             sample_name = row[0]  # First column is the sample name
-            forward_barcode = row[1].split(' ')[0]  # Extract forward barcode from the second column
-            reverse_barcode = row[2].split(' ')[0]  # Extract reverse barcode from the third column
-            barcodes.append((sample_name, forward_barcode, reverse_barcode))  # Store the extracted values
+            forward_barcode = row[1].split(" ")[
+                0
+            ]  # Extract forward barcode from the second column
+            reverse_barcode = row[2].split(" ")[
+                0
+            ]  # Extract reverse barcode from the third column
+            barcodes.append(
+                (sample_name, forward_barcode, reverse_barcode)
+            )  # Store the extracted values
 
         # Define the output FASTA file names
-        samplesheet_base = os.path.splitext(os.path.basename(samplesheet))[0]  # Remove the path and file extension
-        forward_fasta = os.path.join(output_dir, f"barcodes_fwd.fasta")
-        reverse_fasta = os.path.join(output_dir, f"barcodes_bc_rev.fasta")
+        forward_fasta = os.path.join(output_dir, "barcodes_fwd.fasta")
+        reverse_fasta = os.path.join(output_dir, "barcodes_bc_rev.fasta")
 
         # Write the barcodes to the respective FASTA files
-        with open(forward_fasta, 'w') as fwd_file, open(reverse_fasta, 'w') as rev_file:
+        with open(forward_fasta, "w") as fwd_file, open(reverse_fasta, "w") as rev_file:
             for sample_name, forward_barcode, reverse_barcode in barcodes:
-                fwd_file.write(f">{sample_name}\n{forward_barcode}\n")  # Write forward barcode
-                rev_file.write(f">{sample_name}\n{reverse_barcode}\n")  # Write reverse barcode
+                fwd_file.write(
+                    f">{sample_name}\n{forward_barcode}\n"
+                )  # Write forward barcode
+                rev_file.write(
+                    f">{sample_name}\n{reverse_barcode}\n"
+                )  # Write reverse barcode
 
         # Run cutadapt with the generated FASTA files and provided FASTQ files
         run_cutadapt(forward_fasta, reverse_fasta, fq_gz_1, fq_gz_2, output_dir)
--- src/read_stats.py
+++ src/read_stats.py
@@ -9,19 +9,19 @@ def main():
         input_file = sys.argv[1]
         try:
             with open(input_file, 'r') as f:
-                lines = f.readlines()
+                lines = f.readlines() # read file
         except FileNotFoundError:
             print(f"File {input_file} not found.")
             sys.exit(1)
     else:
-        lines = sys.stdin.readlines()
+        lines = sys.stdin.readlines() # stdin
 
     total_reads = 0
     data = []
 
     for line in lines:
-        parts = line.strip().split('\t')
-        if len(parts) != 2:
+        parts = line.strip().split('\t') # split by tab
+        if len(parts) != 2: # if line has more tha 2 columns
             continue
         filename, read_count = parts
         read_count = int(read_count)
@@ -32,7 +32,7 @@ def main():
         print("No reads found.")
         sys.exit(1)
 
-    print("Filename\tRead Count\tRelative Read Count")
+    print("filename\treads\trelative_reads")
     for filename, read_count in data:
         relative_read_count = read_count / total_reads
         print(f"{filename}\t{read_count}\t{relative_read_count:.6f}")
