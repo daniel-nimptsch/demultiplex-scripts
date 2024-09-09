69c0e9d757d5cd419e2f4e3fe8e0492013792ec9
Daniel Nimptsch <danielnimptsch@hotmail.de>
2024-09-09 21:38:22 +0200

    renamed example pipeline to pipeline as it now support input arguments

---
+++ src/pipeline.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+# Function to display help message
+display_help() {
+    echo "Usage: $0 <input_samplesheet> <fastq1> <fastq2> [options]"
+    echo
+    echo "Description:"
+    echo "  This pipeline performs demultiplexing and analysis of paired-end sequencing data."
+    echo "  It processes the input samplesheet, generates barcodes, counts motifs, demultiplexes"
+    echo "  the data, and generates various output files including read counts and an Ampliseq samplesheet."
+    echo
+    echo "Arguments:"
+    echo "  <input_samplesheet>    Path to the input samplesheet"
+    echo "  <fastq1>               Path to the first FASTQ file (R1)"
+    echo "  <fastq2>               Path to the second FASTQ file (R2)"
+    echo
+    echo "Options:"
+    echo "  -o, --output <dir>     Output directory for demultiplexed files (default: ./data/demultiplex)"
+    echo "  -e, --error-rate <rate> Maximum expected error rate (default: 0.14)"
+    echo "  --min-overlap <int>    Minimum overlap length for adapter matching (default: 3)"
+    echo "  --novogene-samplesheet Use this flag if the input is a Novogene samplesheet"
+    echo "  -h, --help             Display this help message and exit"
+    echo
+    echo "Example:"
+    echo "  $0 input_samplesheet.tsv R1.fastq.gz R2.fastq.gz -o output_dir -e 0.1 --min-overlap 5"
+}
+
+# Set default values
+DEMUX_PATH="./data/demultiplex"
+ERROR_RATE=0.14
+MIN_OVERLAP=3
+NOVOGENE_SAMPLESHEET=false
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+    -h | --help)
+        display_help
+        exit 0
+        ;;
+    -o | --output)
+        DEMUX_PATH="$2"
+        shift 2
+        ;;
+    -e | --error-rate)
+        ERROR_RATE="$2"
+        shift 2
+        ;;
+    --min-overlap)
+        MIN_OVERLAP="$2"
+        shift 2
+        ;;
+    --novogene-samplesheet)
+        NOVOGENE_SAMPLESHEET=true
+        shift
+        ;;
+    *)
+        if [ -z "$INPUT_SAMPLESHEET" ]; then
+            INPUT_SAMPLESHEET="$1"
+        elif [ -z "$FASTQ1" ]; then
+            FASTQ1="$1"
+        elif [ -z "$FASTQ2" ]; then
+            FASTQ2="$1"
+        else
+            echo "Error: Unexpected argument '$1'"
+            echo "Use -h or --help for usage information"
+            exit 1
+        fi
+        shift
+        ;;
+    esac
+done
+
+# Check if required arguments are provided
+if [ -z "$INPUT_SAMPLESHEET" ] || [ -z "$FASTQ1" ] || [ -z "$FASTQ2" ]; then
+    echo "Error: Missing required arguments"
+    echo "Use -h or --help for usage information"
+    exit 1
+fi
+
+if [ ! -f "$INPUT_SAMPLESHEET" ]; then
+    echo "Error: Input samplesheet file '$INPUT_SAMPLESHEET' does not exist."
+    exit 1
+fi
+
+if [ ! -f "$FASTQ1" ]; then
+    echo "Error: FASTQ1 file '$FASTQ1' does not exist."
+    exit 1
+fi
+
+if [ ! -f "$FASTQ2" ]; then
+    echo "Error: FASTQ2 file '$FASTQ2' does not exist."
+    exit 1
+fi
+
+INPUT_DIR=$(dirname "$FASTQ1")
+
+echo "(1/10) Counting reads in multiplex data"
+python demultiplex-scripts/src/read_counts.py "$INPUT_DIR/" \
+    >"$INPUT_DIR/read_counts.tsv"
+
+echo "(2/10) Creating directories"
+mkdir -p "$DEMUX_PATH"
+mkdir -p "$DEMUX_PATH/work"
+
+echo "(3/10) Parsing samplesheet and generating barcodes"
+if [ "$NOVOGENE_SAMPLESHEET" = true ]; then
+    python demultiplex-scripts/src/parse_samplesheet_novogene.py \
+        "$INPUT_SAMPLESHEET" \
+        >"$DEMUX_PATH/input_samplesheet.tsv"
+else
+    cp "$INPUT_SAMPLESHEET" "$DEMUX_PATH/input_samplesheet.tsv"
+fi
+python demultiplex-scripts/src/barcodes_to_fasta.py -o "$DEMUX_PATH/work/" \
+    <"$DEMUX_PATH/input_samplesheet.tsv"
+
+echo "(4/10) Concatenating barcodes and primers"
+cat "$DEMUX_PATH/work/barcodes_fwd.fasta" "$DEMUX_PATH/work/barcodes_rev.fasta" \
+    >"$DEMUX_PATH/work/barcodes.fasta"
+cat "$DEMUX_PATH/work/primers_fwd.fasta" "$DEMUX_PATH/work/primers_rev.fasta" \
+    >"$DEMUX_PATH/work/primers.fasta"
+
+echo "(5/10) Counting motifs"
+python demultiplex-scripts/src/motif_counts.py \
+    "$INPUT_DIR/" \
+    "$DEMUX_PATH/work/barcodes.fasta" \
+    "$DEMUX_PATH/work/primers.fasta" \
+    -o "$DEMUX_PATH/work" \
+    >"$INPUT_DIR/motif_counts.tsv"
+
+echo "(6/10) Demultiplexing"
+python demultiplex-scripts/src/demultiplex.py \
+    "$FASTQ1" \
+    "$FASTQ2" \
+    "$DEMUX_PATH/work/barcodes_fwd.fasta" \
+    "$DEMUX_PATH/work/barcodes_rev.fasta" \
+    -o "$DEMUX_PATH/work" \
+    -c \
+    -e "${ERROR_RATE}" \
+    --min-overlap "${MIN_OVERLAP}"
+
+echo "(7/10) Copying patterns"
+python demultiplex-scripts/src/patterns_copy.py -o "$DEMUX_PATH/demux_renamed" <"$DEMUX_PATH/work/patterns.txt"
+
+echo "(8/10) Moving fastq files"
+mkdir -p "$DEMUX_PATH/work/fastqs/"
+mv "$DEMUX_PATH"/work/*.fastq.gz "$DEMUX_PATH/work/fastqs/"
+
+echo "(9/10) Counting reads in demultiplexed data"
+python demultiplex-scripts/src/read_counts.py "$DEMUX_PATH/demux_renamed/" \
+    >"$DEMUX_PATH/demux_renamed/read_counts.tsv"
+python demultiplex-scripts/src/read_counts.py "$DEMUX_PATH/work/fastqs/" \
+    >"$DEMUX_PATH/work/fastqs/read_counts.tsv"
+
+echo "(10/10) Generating ampliseq samplesheet"
+python demultiplex-scripts/src/ampliseq_samplesheet_gen.py "$DEMUX_PATH/demux_renamed" \
+    >"$DEMUX_PATH/demux_renamed/sample_sheet.csv"
+
+echo "Pipeline completed successfully!"
