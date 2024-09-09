b75711782e4405b5284a2b7789483e3e760c700a
Daniel Nimptsch <danielnimptsch@hotmail.de>
2024-09-09 15:40:16 +0200

    reworked example pipeline

--- src/example_pipeline.sh
+++ src/example_pipeline.sh
@@ -1,24 +1,66 @@
 #!/bin/bash
 
-python src/parse_samplesheet_novogene.py PATH_TO_NOVOGENE_SAMPLESHEET | \
-    python src/barcodes_to_fasta.py -o data/demultiplex
+# Modify these variables
+FASTQ1="path/to/fastq1"
+FASTQ2="path/to/fastq1"
+DEMUX_PATH="path/to/output/dir"
 
-python src/demultiplex.py \
-    PATH_TO_FWD_FASTQ \
-    PATH_TO_REV_FASTQ \
-    data/demultiplex/barcodes_fwd.fasta \
-    data/demultiplex/barcodes_rev.fasta \
-    -o data/demultiplex \
+# Demultiplex pipeline start:
+INPUT_DIR=$(dirname "$FASTQ1")
+
+echo "(1/10) Counting reads in multiplex data"
+python demultiplex-scripts/src/read_counts.py "$INPUT_DIR/" \
+    >"$INPUT_DIR/read_counts.tsv"
+
+echo "(2/10) Creating directories"
+mkdir -p "$DEMUX_PATH"
+mkdir -p "$DEMUX_PATH/work"
+
+echo "(3/10) Parsing samplesheet and generating barcodes"
+python demultiplex-scripts/src/parse_samplesheet_novogene.py \
+    "$INPUT_DIR/multiplex_samplesheet.tsv" \
+    >"$DEMUX_PATH/input_samplesheet.tsv"
+python demultiplex-scripts/src/barcodes_to_fasta.py -o "$DEMUX_PATH/work/" \
+    <"$DEMUX_PATH/input_samplesheet.tsv"
+
+echo "(4/10) Concatenating barcodes and primers"
+cat "$DEMUX_PATH/barcodes_fwd.fasta" "$DEMUX_PATH/work/barcodes_rev.fasta" \
+    >"$DEMUX_PATH/work/barcodes.fasta"
+cat "$DEMUX_PATH/primers_fwd.fasta" "$DEMUX_PATH/work/primers_rev.fasta" \
+    >"$DEMUX_PATH/work/primers.fasta"
+
+echo "(5/10) Counting motifs"
+python demultiplex-scripts/src/motif_counts.py \
+    "$INPUT_DIR/" \
+    "$DEMUX_PATH/work/barcodes.fasta" \
+    "$DEMUX_PATH/work/primers.fasta" \
+    -o "$DEMUX_PATH/work" \
+    >"$INPUT_DIR/motif_counts.tsv"
+
+echo "(6/10) Demultiplexing"
+python demultiplex-scripts/src/demultiplex.py \
+    "$FASTQ1" \
+    "$FASTQ2" \
+    "$DEMUX_PATH/work/barcodes_fwd.fasta" \
+    "$DEMUX_PATH/work/barcodes_rev.fasta" \
+    -o "$DEMUX_PATH" \
     -c
 
-cat data/demultiplex/patterns.txt | \
-    python src/patterns_copy.py -o data/demultiplex/demux_renamed
+echo "(7/10) Copying patterns"
+python demultiplex-scripts/src/patterns_copy.py -o "$DEMUX_PATH/demux_renamed" <"$DEMUX_PATH/work/patterns.txt"
 
-mkdir data/demultiplex/work
-mv data/demultiplex/*.fastq.gz data/demultiplex/work/
+echo "(8/10) Moving fastq files"
+mkdir -p "$DEMUX_PATH/work/fastqs/"
+mv "$DEMUX_PATH"/*.fastq.gz "$DEMUX_PATH/work/fastqs/"
 
-bash src/dir_to_reads_tsv.sh data/demultiplex/work > data/demultiplex/cutadapt_reads.tsv
-bash src/dir_to_reads_tsv.sh data/demultiplex/demux_renamed > data/demultiplex/sample_reads.tsv
+echo "(9/10) Counting reads in demultiplexed data"
+python demultiplex-scripts/src/read_counts.py "$DEMUX_PATH/demux_renamed/" \
+    >"$DEMUX_PATH/demux_renamed/read_counts.tsv"
+python demultiplex-scripts/src/read_counts.py "$DEMUX_PATH/work/fastqs/" \
+    >"$DEMUX_PATH/work/fastqs/read_counts.tsv"
 
-python src/ampliseq_samplesheet_gen.py data/demultiplex/demux_renamed > \
-    data/demultiplex/demux_renamed/sample_sheet.csv
+echo "(10/10) Generating ampliseq samplesheet"
+python demultiplex-scripts/src/ampliseq_samplesheet_gen.py "$DEMUX_PATH/demux_renamed" \
+    >"$DEMUX_PATH/demux_renamed/sample_sheet.csv"
+
+echo "Pipeline completed successfully!"
